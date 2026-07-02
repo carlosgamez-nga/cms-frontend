2fdbb81ab34267ab77fc740f03ae6826eefad4f0
justin-nga <justin@ngahealthcare.com>
2026-07-02 15:50:50 -0600

    whack a mole continues

--- src/app/dashboard/components/cards/cards-grid.tsx
+++ src/app/dashboard/components/cards/cards-grid.tsx
@@ -3,10 +3,10 @@ import CardOverview from './card-overview';
 const CardsGrid = () => {
   return (
     <div className='grid gap-6 md:grid-cols-2 lg:grid-cols-4'>
-      <CardOverview />
-      <CardOverview />
-      <CardOverview />
-      <CardOverview />
+        <CardOverview title="Total Contracts" amount="1,245" percentage="+15%" />
+        <CardOverview title="Active Payers" amount="42" percentage="+5%" />
+        <CardOverview title="Pending Uploads" amount="18" percentage="-2%" />
+        <CardOverview title="Expiring Soon" amount="4" percentage="0%" />
     </div>
   );
 };
